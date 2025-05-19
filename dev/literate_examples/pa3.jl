8a8bda8447d9649144553d1e7bb27fd4c7066ce9
Documenter.jl <documenter@juliadocs.github.io>
2025-05-19 22:04:46 +0000

    build based on 73fc07a

--- dev/literate_examples/pa3.jl
+++ dev/literate_examples/pa3.jl
@@ -1,9 +1,18 @@
 # # PA-Three
 
+# ## Problem
+
+# PA-Three is an example in [ehrhardt-sun-zheng2009](@cite)
+# + ``q(x_{1}, x_{2}) = 1 + 0.5 \cos(2\pi x_{1}) \sin(2\pi x_{2})``
+# + Neumann boundary condition on ``\partial \Omega``
+
+# ## Code
+
+# Load the necessary packages
 using Ferrite: Lagrange, RefTriangle
 using ClosedWaveguideDispersion
 
-# We define the parameters
+# In this example, we only need to define a new refractive index.
 function n(x)
     return 1.0 + 0.5 * cos(2π * x[1]) * sin(2π * x[2])
 end;
@@ -16,8 +25,10 @@ grid = setup_grid(lc=0.05, period=p, height=h)
 
 ## Define the interpolation 
 ip = Lagrange{RefTriangle, 1}()
+
 ## Set up the FE values: CellValues 
 cv = setup_fevs(ip)
+
 ## Set up the DofHandler 
 dh = setup_dofs(grid, ip)
 
