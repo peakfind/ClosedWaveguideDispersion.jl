8295203243ecfe60a67109846d45d1d03f67cef5
Documenter.jl <documenter@juliadocs.github.io>
2025-05-19 15:28:39 +0000

    build based on fb69a7b

--- dev/literate_examples/pa2.jl
+++ dev/literate_examples/pa2.jl
@@ -1,4 +1,9 @@
-# pa2.jl
+# # PA-Two
+
+# ## Problem
+
+# ## Code
+# For this example, we also need to load Gmsh and FerriteGmsh to generate the mesh
 using Gmsh
 using Ferrite
 using FerriteGmsh
@@ -6,11 +11,11 @@ using ClosedWaveguideDispersion
 
 # We need to customize our mesh and boundary conditions
 function my_grid(;lc=0.05, period=1.0, height=1.0, holewidth=0.5, holeheight=0.5)
-    # Initialize gmsh 
+    ## Initialize gmsh 
     gmsh.initialize()
     gmsh.option.setNumber("General.Verbosity", 2)
     
-    # Add the points 
+    ## Add the points 
     p1 = gmsh.model.geo.addPoint(-period/2, 0, 0, lc)
     p2 = gmsh.model.geo.addPoint(period/2, 0, 0, lc)
     p3 = gmsh.model.geo.addPoint(period/2, height, 0, lc)
@@ -20,7 +25,7 @@ function my_grid(;lc=0.05, period=1.0, height=1.0, holewidth=0.5, holeheight=0.5
     p7 = gmsh.model.geo.addPoint(holewidth/2, (height + holeheight)/2, 0, lc)
     p8 = gmsh.model.geo.addPoint(-holewidth/2, (height + holeheight)/2, 0, lc)
     
-    # Add the lines 
+    ## Add the lines 
     l1 = gmsh.model.geo.addLine(p1, p2)
     l2 = gmsh.model.geo.addLine(p2, p3)
     l3 = gmsh.model.geo.addLine(p3, p4)
@@ -30,15 +35,15 @@ function my_grid(;lc=0.05, period=1.0, height=1.0, holewidth=0.5, holeheight=0.5
     l7 = gmsh.model.geo.addLine(p7, p8)
     l8 = gmsh.model.geo.addLine(p8, p5)
     
-    # Create loops and the domain 
+    ## Create loops and the domain 
     outerLoop = gmsh.model.geo.addCurveLoop([l1, l2, l3, l4])
     innerLoop = gmsh.model.geo.addCurveLoop([l5, l6, l7, l8])
     domain = gmsh.model.geo.addPlaneSurface([outerLoop, innerLoop])
     
-    # Synchronize the model
+    ## Synchronize the model
     gmsh.model.geo.synchronize()
 
-    # Create the physical domains 
+    ## Create the physical domains 
     gmsh.model.addPhysicalGroup(1, [l1], -1, "bottom")
     gmsh.model.addPhysicalGroup(1, [l2], -1, "right")
     gmsh.model.addPhysicalGroup(1, [l3], -1, "top")
@@ -46,11 +51,11 @@ function my_grid(;lc=0.05, period=1.0, height=1.0, holewidth=0.5, holeheight=0.5
     gmsh.model.addPhysicalGroup(1, [l5, l6, l7, l8], -1, "Γ")
     gmsh.model.addPhysicalGroup(2, [domain], -1, "Ω")
 
-    # Set Periodic boundary condition
+    ## Set Periodic boundary condition
     transform = [1, 0, 0, period, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]
     gmsh.model.mesh.setPeriodic(1, [l2], [l4], transform) 
 
-    # Generate a 2D mesh
+    ## Generate a 2D mesh
     gmsh.model.mesh.generate(2)
 
     grid = mktempdir() do dir 
@@ -59,26 +64,27 @@ function my_grid(;lc=0.05, period=1.0, height=1.0, holewidth=0.5, holeheight=0.5
         togrid(path)
     end
    
-    # Finalize the Gmsh library
+    ## Finalize the Gmsh library
     gmsh.finalize()
     
     return grid
-end 
+end;
 
 function n(x)
     return 1.0
-end
+end;
 
+# We need to implement a new function to impose the boundary condition.
 function my_bdcs(dh::DofHandler; period=1.0)
     cst = ConstraintHandler(dh)
     
-    # Periodic boundary condition on the left and right 
-    # side of the periodic cell
+    ## Periodic boundary condition on the left and right 
+    ## side of the periodic cell
     pfacets = collect_periodic_facets(dh.grid, "right", "left", x -> x + Ferrite.Vec{2}((period, 0.0)))
     pbc = PeriodicDirichlet(:u, pfacets)
     add!(cst, pbc)
     
-    # Set Dirichlet boundary condition on the inner boundary
+    ## Set Dirichlet boundary condition on the inner boundary
     dfacets = getfacetset(dh.grid, "Γ")
     dbc = Dirichlet(:u, dfacets, x -> 0)
     add!(cst, dbc)
@@ -86,36 +92,36 @@ function my_bdcs(dh::DofHandler; period=1.0)
     close!(cst)
 end
 
-p = 1.0
-h = 1.0
-hw = 0.5
-hh = 0.5
-N = 150
+# Set parameters
+p = 1.0;
+h = 1.0;
+hw = 0.5;
+hh = 0.5;
+N = 150;
 
 # Set up the grid 
 grid = my_grid(lc=0.05, period=p, height=h, holewidth=hw, holeheight=hh)
 
-# Define the interpolation 
+# basic steps need by Ferrite.jl
+## Define the interpolation 
 ip = Lagrange{RefTriangle, 1}()
-
-# Set up the FE values: CellValues 
+## Set up the FE values: CellValues 
 cv = setup_fevs(ip)
-
-# Set up the DofHandler 
+## Set up the DofHandler 
 dh = setup_dofs(grid, ip)
 
-# Set the boundary conditions
+## Set the boundary conditions
 cst = my_bdcs(dh, period=p)
 
-# Allocate the matrices
+## Allocate the matrices
 A = allocate_matries(dh, cst)
 B = allocate_matries(dh, cst)
 
-# Discretize the Brillouin zone
+## Discretize the Brillouin zone
 bz = collect(range(-π/p, 2π/p, N))
 
-# Calculate the dispersion diagram
+## Calculate the dispersion diagram
 μ = calc_diagram(cv, dh, cst, A, B, n, bz, nevs=5)
 
-# Plot the dispersion diagram
+## Plot the dispersion diagram
 plot_diagram(bz, μ, period=p)
