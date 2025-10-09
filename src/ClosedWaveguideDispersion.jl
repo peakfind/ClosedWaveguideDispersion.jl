564f76f1d9f355cf50913e244dd9237eb0ff7d63
jyzhang <peakfind@126.com>
2025-10-09 10:13:07 +0200

    Export functions for bi-periodic problems

--- src/ClosedWaveguideDispersion.jl
+++ src/ClosedWaveguideDispersion.jl
@@ -9,12 +9,20 @@ using CairoMakie
 using StaticArrays
 
 include("fem.jl")
+# For 1D periodicity
 export setup_grid, setup_fevs, setup_dofs, setup_bdcs
 export allocate_matries, assemble_A, assemble_B
+
+# For 2D periodicity
 export setup_grid_squareLattice
+export assemble_A_TE, assemble_A_TM, assemble_B_TE, assemble_B_TM
 
 include("evp_solver.jl")
+# For 1D periodicity
 export calc_diagram
+
+# For 2D periodicity
+export calc_diagram_TE, calc_diagram_TM
 
 include("plot.jl")
 export plot_diagram
